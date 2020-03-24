ef5e6da47bcf05ee7da10730c43f37b0ff8fdc09
Jose Luis Cahuana <joseluis.cahuana@hotmail.com>
2020-03-24 09:42:40 -0500

    Updates

---
+++ precache-manifest.b172f3560acf8b82bef3d94d4b97a84c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ee6e538497b6d86fd58b6087dede91",
+    "url": "//jona35q.github.io/app-citas/index.html"
+  },
+  {
+    "revision": "6a058a1c0a0b4d5b79b2",
+    "url": "//jona35q.github.io/app-citas/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "07b7d688c4a74adee956",
+    "url": "//jona35q.github.io/app-citas/static/js/2.bfe11fc5.chunk.js"
+  },
+  {
+    "revision": "6a058a1c0a0b4d5b79b2",
+    "url": "//jona35q.github.io/app-citas/static/js/main.6fddd264.chunk.js"
+  },
+  {
+    "revision": "464e2983ef445a23baf2",
+    "url": "//jona35q.github.io/app-citas/static/js/runtime-main.871e80e6.js"
+  }
+]);
